ea2f291943d9c40b09a32de00dbc5292a0395992
Brijesh Shetty <brij.jhu@gmail.com>
2022-08-12 11:25:34 -0700

    Reduce bootstrap script stdout

--- scripts/bootstrap_udacity.sh
+++ scripts/bootstrap_udacity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Verifying virtual_envs"
-ls -lh /data/virtual_envs/
+ls -lh /data/virtual_envs/ 2> /dev/null
 
 echo "Setting up vscode extensions & user-data dir"
 mkdir -p /home/workspace/.vscode/user-data/
@@ -10,15 +10,16 @@ echo "code --user-data-dir /home/workspace/.vscode/user-data/ --extensions-dir /
 chmod u+x ./code.sh
 
 echo "vs code is set to run using custom user-data-dir and extensions-dir"
-
+echo "\n"
 echo "installing vs code launch.json & settings.json"
 # download launch.json (Vscode debug settings)
-curl https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/launch.json --output /home/workspace/.vscode/launch.json
+curl -s https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/launch.json --output /home/workspace/.vscode/launch.json
 
 # download User settings.json
-curl https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/user_data_user_settings.json --output /home/workspace/.vscode/user-data/User/settings.json
+curl -s https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/user_data_user_settings.json --output /home/workspace/.vscode/user-data/User/settings.json
 
 echo "Done"
+echo "\n"
 echo " Run the following to launch vs code"
 echo " ./code.sh /home/workspace/"
 
