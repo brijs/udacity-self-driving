fcb1fad11278725dc2317c2668e1a336da62c53d
Brijesh Shetty <brij.jhu@gmail.com>
2022-08-13 13:21:46 -0700

    Echo to stdout on failure

--- scripts/bootstrap_udacity.sh
+++ scripts/bootstrap_udacity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Verifying virtual_envs"
-ls -lh /data/virtual_envs/ 2> /dev/null
+ls -lh /data/virtual_envs/ 2> /dev/null 
 
 echo "Setting up vscode extensions & user-data dir"
 mkdir -p /home/workspace/.vscode/user-data/
@@ -11,12 +11,13 @@ chmod u+x ./code.sh
 
 echo "vs code is set to run using custom user-data-dir and extensions-dir"
 echo "\n"
-echo "installing vs code launch.json & settings.json"
+echo "Installing vs code launch.json & settings.json"
 # download launch.json (Vscode debug settings)
-curl -s https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/launch.json --output /home/workspace/.vscode/launch.json
+curl -s https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/launch.json --output /home/workspace/.vscode/launch.json || echo "FAILED: installing launch.json"
 
 # download User settings.json
-curl -s https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/user_data_user_settings.json --output /home/workspace/.vscode/user-data/User/settings.json
+mkdir -p /home/workspace/.vscode/user-data/User
+curl -s https://raw.githubusercontent.com/brijs/udacity-self-driving/main/scripts/user_data_user_settings.json --output /home/workspace/.vscode/user-data/User/settings.json || echo "FAILED: installing user-data settings.json"
 
 echo "Done"
 echo "\n"
